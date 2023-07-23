f937683f38b06ba249a8a39c6da248c35550664b
kevink5444 <kevinkurniawan901@gmail.com>
2023-07-23 14:27:01 +0700

    artikel

--- src/components/OtherBlog.jsx
+++ src/components/OtherBlog.jsx
@@ -30,13 +30,13 @@ const OtherBlog = () => {
         <h1 className="text-3xl font-bold mb-4 text-center">Other Blogs</h1>
         {data.map((blog, i) => (
           <div key={i} className="flex mb-4 p-6 rounded-[20px] bg-[#D9D9D9] shadow-xl">
-            <img className="w-1/4 mr-4 rounded-xl" src={blog.thumbnail} alt={blog.judul} />
+            <img className="w-1/4 mr-4 rounded-xl" src={blog.imageUrl} alt={blog.title} />
             <div className="flex flex-col">
-              <h2 className="text-xl font-bold mb-2">{blog.judul}</h2>
-              <p className="text-gray-700 text-justify text-ellipsis overflow-hidden">{blog.ringkasan.length > 100
-          ? `${blog.ringkasan.substring(0, 100)}...`
-          : blog.ringkasan}</p>
-              <Link to={`/${blog.judul}`}>
+              <h2 className="text-xl font-bold mb-2">{blog.title}</h2>
+              <p className="text-gray-700 text-justify text-ellipsis overflow-hidden">{blog.description.length > 100
+          ? `${blog.description.substring(0, 100)}...`
+          : blog.description}</p>
+              <Link to={`/${blog.title}`}>
               <button className="bg-[#f48c06] text-white w-[100px] h-[40px] rounded-xl hover:bg-[#252641] text-white">
                   Read More
               </button>
